57cf2c4d162274714a7c0d94c3347b2e50d9c883
smallljl <168241296@qq.com>
2020-07-23 18:01:49 +0800

    animation

--- week15/ANIMAION/animation.js
+++ week15/ANIMAION/animation.js
@@ -2,13 +2,14 @@ class Timeline {
     constructor(){
       this.animations = [];
       this.requestID = null;
+      this.state = "inited";
       this.tick = () => {
         let t =  Date.now() - this.startTime;
         let animations = this.animations.filter(animation => !animation.finished);
         for(let animation of this.animations){
-          let {object,property,start,end,timingFunction,duration,template,delay} = animation;
-          let progression = timingFunction((t - delay)/duration); // 0 - 1 之间的数
-          if(t > animation.duration + animation.delay){ // 到末尾了
+          let {object,property,start,end,timingFunction,duration,addTime,template,delay} = animation;
+          let progression = timingFunction((t - delay - addTime )/duration); // 0 - 1 之间的数
+          if(t > duration + delay + addTime){ // 到末尾了
             progression = 1;
             animation.finished = true;
           }
@@ -20,15 +21,48 @@ class Timeline {
       }
     }
     pause(){
+      if(this.state !== "playing")
+        return;
+      this.state = "paused";
+      this.pauseTime = Date.now();
       if(this.requestID !== null)
         cancelAnimationFrame(this.requestID);
     }
+
+    resume(){
+      if(this.state !== "paused")
+        return;
+      this.state = "resumed";
+      this.startTime += Date.now() - this.pauseTime; // 扣除暂停的时间
+      this.tick();
+    }
+
     start(){
+      if(this.state !== "inited")
+        return;
+      this.state = "playing";
       this.startTime = Date.now();
       this.tick();
     }
-    add(animation){
+
+    restart(){
+      if(this.state === "playing")
+        this.pause();
+      this.animations = [];
+      this.requestID = null;
+      this.state = "playing";
+      this.startTime = Date.now();
+      this.pauseTime = null;
+      this.tick();
+    }
+
+    add(animation,addTime){
       this.animations.push(animation);
+      animation.finished = false;
+      if(this.state === "playing")
+        animation.addTime = addTime !== void 0 ?  addTime : Date.now() - this.startTime;
+      else
+        animation.addTime = addTime !== void 0 ?  addTime : 0;
     }
   }
   
