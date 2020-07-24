d9a12956c81103ab04a44b72aa84dba207a58bac
smallljl <168241296@qq.com>
2020-07-24 10:24:51 +0800

    week15

--- week15/ANIMAION/animation.js
+++ week15/ANIMAION/animation.js
@@ -7,13 +7,14 @@ class Timeline {
         let t =  Date.now() - this.startTime;
         let animations = this.animations.filter(animation => !animation.finished);
         for(let animation of this.animations){
-          let {object,property,start,end,timingFunction,duration,addTime,template,delay} = animation;
+          let {object,property,timingFunction,duration,addTime,template,delay} = animation;
           let progression = timingFunction((t - delay - addTime )/duration); // 0 - 1 之间的数
           if(t > duration + delay + addTime){ // 到末尾了
             progression = 1;
             animation.finished = true;
           }
-          let value = start + progression * (end - start); // value 就是根据progression算出当前的位置
+          let value = animation.valueFromProgression(progression); // value 就是根据progression算出当前的位置
+         
           object[property] = template(value);
         }
         if(animations.length)
@@ -67,7 +68,7 @@ class Timeline {
   }
   
 class Animation {
-    constructor(object,property,template, start,end,duration,delay,timingFunction){
+    constructor(object,property,start,end,duration,delay,timingFunction,template){
       this.object = object;
       this.template = template;
       this.property = property;
@@ -78,7 +79,33 @@ class Animation {
       this.timingFunction = timingFunction;
       // ease linear easeIn easeOut
     }
+    valueFromProgression(progression){
+      return this.start + progression * (this.end - this.start)
+    }
   }
+
+  class ColorAnimation {
+    constructor(object,property,start,end,duration,delay,timingFunction,template){
+      this.object = object;
+      this.template = template || (v =>`rgba(${v.r},${v.g},${v.b},${v.a})`);
+      this.property = property;
+      this.start = start;
+      this.end = end;
+      this.duration = duration;
+      this.delay = delay;  // 延迟
+      this.timingFunction = timingFunction;
+      // ease linear easeIn easeOut
+    }
+    valueFromProgression(progression){
+      return {
+        r: parseInt(this.start.r + progression * (this.end.r - this.start.r)) ,
+        g: parseInt(this.start.g + progression * (this.end.g - this.start.g)),
+        b: parseInt(this.start.r + progression * (this.end.b - this.start.b)),
+        a: parseInt(this.start.a + progression * (this.end.a - this.start.a)),
+      }
+    }
+  }
+
   /*
     let animation = new Animation(object,property,start,end,duration,delay,timingFunction);
     let animation2 = new Animation(object,property,start,end,duration,delay,timingFunction);
