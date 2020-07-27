9e01862c062527d13a207d943919ea413769da2b
smallljl <168241296@qq.com>
2020-07-27 19:26:21 +0800

    16周

--- week16/gesture.js
+++ week16/gesture.js
@@ -1,77 +1,125 @@
 let element = document.body;
 
-let contexts = Object.create(null);
+function enableGesture(element){
+  let contexts = Object.create(null);
 
-let MOUSE_SYMBOL = Symbol("mouse");
+  let MOUSE_SYMBOL = Symbol("mouse");
 
+  if(!document.ontouchstart !== null)
+    element.addEventListener("mousedown",(event)=>{
+      contexts[MOUSE_SYMBOL] = Object.create(null);
+      start(event,contexts[MOUSE_SYMBOL]);
+      let mousemove = event => {
+        move(event,contexts[MOUSE_SYMBOL]);
+      }
+      let mouseend = event => {
+        end(event,contexts[MOUSE_SYMBOL]);
+        document.removeEventListener("mousemove",mousemove);
+        document.removeEventListener("mouseup",mouseend);
+      }
+      document.addEventListener("mousemove",mousemove);
+      document.addEventListener("mouseup",mouseend);
+    });
 
-element.addEventListener("mousedown",(event)=>{
-  contexts[MOUSE_SYMBOL] = Object.create(null);
-  start(event,contexts[MOUSE_SYMBOL]);
-  let mousemove = event => {
-    move(event,contexts[MOUSE_SYMBOL]);
+  element.addEventListener("touchstart",event => {
+    for(let touch of event.changedTouches){
+      contexts[touch.identifier] = Object.create(null);
+      start(touch,contexts[touch.identifier]);
+    }
+  });
+
+  element.addEventListener("touchmove",event=>{
+    for(let touch of event.changedTouches){
+      move(touch,contexts[touch.identifier]);
+    }
+  });
+  
+  element.addEventListener("touchend",event=>{
+    for(let touch of event.changedTouches){
+      end(touch,contexts[touch.identifier]);
+      delete contexts[touch.identifier];
+    }
+  });
+
+  element.addEventListener("touchcancel",event => {
+    for(let touch of event.changedTouches){
+      cancel(touch,contexts[touch.identifier]);
+      delete contexts[touch.identifier];
+    }
+  });
+
+  //tap
+  //pan - panstart panmove panend
+  //flick 
+  //press - pressstart pressend
+
+  let start = (point,context) => {
+    context.startX = point.clientX,context.startY = point.clientY;
+    context.moves = [];
+    context.isTap = true;
+    context.isPan = false;
+    context.isPress = false;
+    context.timeoutHandler = setTimeout(()=>{
+      if(context.isPan)
+        return;
+
+      context.isTap = false;
+      context.isPan = false;
+      context.isPress = true;
+    },500)
+    // console.log("start",point.clientX,point.clientY);
   }
-  let mouseend = event => {
-    end(event,contexts[MOUSE_SYMBOL]);
-    document.removeEventListener("mousemove",mousemove);
-    document.removeEventListener("mouseup",mouseend);
+
+  let move = (point,context) => {
+    let dx = point.clientX - context.startX, dy = point.clientY - context.startY;
+    if(dx ** 2 + dy ** 2 > 100 && !context.isPan){
+      if(context.isPress)
+        console.log("presscancel");
+      context.isTap = false;
+      context.isPan = true;
+      context.isPress = false;
+
+      console.log("panstart");
+    }
+
+    
+
+    context.moves = context.moves.filter(record => Date.now() - record.t < 300);  // 大于300的保存
+
+    if(context.isPan){
+      context.moves.push({
+        dx,dy,
+        t:Date.now()
+      });
+      console.log("pan");
+    }
   }
-  document.addEventListener("mousemove",mousemove);
-  document.addEventListener("mouseup",mouseend);
-});
 
-element.addEventListener("touchstart",event => {
-  for(let touch of event.changedTouches){
-    contexts[touch.identifier] = Object.create(null);
-    start(touch,contexts[touch.identifier]);
+  let end = (point,context) => {
+    if(context.isPan){
+      let dx = point.clientX - context.startX,dy = point.clientY - context.startY;
+      // console.log(context.moves);
+      let record = context.moves[0];
+      let speed = Math.sqrt((record.dx -dx) ** 2 + (record.dy - dy) ** 2) / (Date.now() - record.t);
+      if(speed > 2.5){
+        console.log("flick");
+      }
+      console.log("panend");
+    }
+    if(context.isTap)
+      console.log("tapend");
+    if(context.isPress)
+      console.log("pressend");
+    clearTimeout(context.timeoutHandler);
   }
-});
 
-element.addEventListener("touchmove",event=>{
-  for(let touch of event.changedTouches){
-    move(touch,contexts[touch.identifier]);
+  let cancel = () => {
+    console.log("canceled");
+    clearTimeout(context.timeoutHandler);
   }
-});
- 
-element.addEventListener("touchend",event=>{
-  for(let touch of event.changedTouches){
-    end(touch,contexts[touch.identifier]);
-    delete contexts[touch.identifier];
-  }
-});
-
-element.addEventListener("touchcancel",event => {
-  for(let touch of event.changedTouches){
-    cancel(touch,contexts[touch.identifier]);
-     delete contexts[touch.identifier];
-  }
-});
-
-
-//tap
-//pan - panstart panmove panend
-//flick 
-//press - pressstart pressend
-
-
-let start = (point,context) => {
-  context.startX = point.clientX,context.startY = point.clientY;
-  context.isTap = true;
-  context.isPan = false;
-  context.isPress = false;
-  setTimeout()
-  console.log("start",point.clientX,point.clientY);
 }
 
-let move = (point,context) => {
-  let dx = point.clientX - context.startX, dy = point.clientY - context.startY;
-  console.log("move",dx,dy);
-}
 
-let end = (point,context) => {
-  console.log("end",point.clientX,point.clientY);
-}
 
-let cancel = () => {
-  console.log("cancel");
-}
+
+
