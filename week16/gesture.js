31f729ea2d7f90c53466c7b4beb51f9261b547cf
smallljl <168241296@qq.com>
2020-07-28 17:04:03 +0800

    gesture

--- week16/gesture.js
+++ week16/gesture.js
@@ -1,11 +1,10 @@
-let element = document.body;
-
 function enableGesture(element){
+
   let contexts = Object.create(null);
 
   let MOUSE_SYMBOL = Symbol("mouse");
 
-  if(!document.ontouchstart !== null)
+  if(!document.ontouchstart !== null)  // PC 和 触摸板要一个
     element.addEventListener("mousedown",(event)=>{
       contexts[MOUSE_SYMBOL] = Object.create(null);
       start(event,contexts[MOUSE_SYMBOL]);
@@ -54,6 +53,13 @@ function enableGesture(element){
   //press - pressstart pressend
 
   let start = (point,context) => {
+    element.dispatchEvent(new CustomEvent("start",{
+      startX:point.clientX,
+      startY:point.clientY,
+      clientX:point.clientX,
+      clientY:point.clientY
+    }));
+
     context.startX = point.clientX,context.startY = point.clientY;
     context.moves = [];
     context.isTap = true;
@@ -62,59 +68,87 @@ function enableGesture(element){
     context.timeoutHandler = setTimeout(()=>{
       if(context.isPan)
         return;
-
       context.isTap = false;
       context.isPan = false;
       context.isPress = true;
+      element.dispatchEvent(new CustomEvent("pressstart",{}));
     },500)
     // console.log("start",point.clientX,point.clientY);
   }
 
   let move = (point,context) => {
     let dx = point.clientX - context.startX, dy = point.clientY - context.startY;
+
+    // 不是isPan的时候触一次
     if(dx ** 2 + dy ** 2 > 100 && !context.isPan){
       if(context.isPress)
-        console.log("presscancel");
+        element.dispatchEvent(new CustomEvent("presscancel",{}));
+
       context.isTap = false;
       context.isPan = true;
       context.isPress = false;
 
-      console.log("panstart");
+      element.dispatchEvent(Object.assign(new CustomEvent("panstart") ,{
+        startX:context.startX,
+        startY:context.startY,
+        clientX:point.clientX,
+        clientY:point.clientY
+      }));
     }
-
-    
-
-    context.moves = context.moves.filter(record => Date.now() - record.t < 300);  // 大于300的保存
 
     if(context.isPan){
       context.moves.push({
         dx,dy,
         t:Date.now()
       });
-      console.log("pan");
+      element.dispatchEvent(Object.assign(new CustomEvent("pan"),{
+        startX:context.startX,
+        startY:context.startY,
+        clientX:point.clientX,
+        clientY:point.clientY
+      }));
     }
+    context.moves = context.moves.filter(record => Date.now() - record.t < 300);  // 小于300的保存
   }
 
   let end = (point,context) => {
     if(context.isPan){
       let dx = point.clientX - context.startX,dy = point.clientY - context.startY;
-      // console.log(context.moves);
       let record = context.moves[0];
       let speed = Math.sqrt((record.dx -dx) ** 2 + (record.dy - dy) ** 2) / (Date.now() - record.t);
-      if(speed > 2.5){
-        console.log("flick");
+      let isFlick = speed > 2.5
+      if(isFlick ){
+        element.dispatchEvent(Object.assign(new CustomEvent("flick"),{
+          startX:context.startX,
+          startY:context.startY,
+          clientX:point.clientX,
+          clientY:point.clientY,
+          speed:speed
+        }));
+        // console.log("flick");
       }
-      console.log("panend");
+      element.dispatchEvent(Object.assign(new CustomEvent("panend"),{
+        startX:context.startX,
+        startY:context.startY,
+        clientX:point.clientX,
+        clientY:point.clientY,
+        speed:speed,
+        isFlick:isFlick
+      }));
+      // console.log("panend");
     }
-    if(context.isTap)
-      console.log("tapend");
+    if(context.isTap){
+      element.dispatchEvent(new CustomEvent("tap",{}))
+      // console.log("tapend");
+    }
     if(context.isPress)
-      console.log("pressend");
+      element.dispatchEvent(new CustomEvent("pressend",{}))
+
     clearTimeout(context.timeoutHandler);
   }
 
   let cancel = () => {
-    console.log("canceled");
+    element.dispatchEvent(new CustomEvent("canceled",{}))
     clearTimeout(context.timeoutHandler);
   }
 }
