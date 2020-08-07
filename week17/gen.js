90e40b52bf19aaedb4f342abe8c1241fec2e8b5f
smallljl <168241296@qq.com>
2020-08-07 10:52:13 +0800

    gen

--- week17/gen.js
+++ week17/gen.js
@@ -3,3 +3,25 @@ function gen(){
 }
 
 let f = (x) => x * x;
+
+
+(g => 
+  (f => f(f))(
+    self => g((...args)=>self(self).apply(this,args))
+  ))(
+  self => {
+    return n => n > 0 ? self(n - 1) + n : 0 
+  }
+)(100);
+
+
+let y = g => 
+    (f => f(f))(
+      self => g((...args)=>self(self).apply(this,args))
+    )
+
+let f = y(self => {
+  return n => n > 0 ? self(n - 1) + n : 0 
+})
+
+f(100);
