61a6bf0a26c796b81c1ad790385e0625544d964c
smallljl <168241296@qq.com>
2020-08-10 15:49:25 +0800

    yeo-man

---
+++ week17/tools/generator-winter/generators/app/index.js
@@ -0,0 +1,35 @@
+let Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+  // note: arguments and options should be defined in the constructor.
+  constructor(args, opts) {
+    super(args, opts);
+  }
+
+  async prompting() {
+    this.dependency = await this.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Would you like to enable the Cool feature?"
+      }
+    ]);
+  }
+
+  writing() {
+    const pkgJson = {
+      devDependencies: {
+        [this.dependency.name]: '*'
+      },
+    };
+
+    // Extend or create package.json file in destination path
+    this.fs.extendJSON(this.destinationPath('package.json'), pkgJson);
+  }
+
+  install() {
+    this.npmInstall();
+  }
+
+ 
+};
