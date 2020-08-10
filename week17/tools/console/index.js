a478b8c6cb5014034ae0c8ca646cea2d6bef8b64
smallljl <168241296@qq.com>
2020-08-10 19:57:29 +0800

    process

---
+++ week17/tools/console/index.js
@@ -0,0 +1,24 @@
+var stdin = process.stdin;
+
+stdin.setRawMode( true );
+stdin.resume();
+stdin.setEncoding( 'utf8' );
+
+function getChar(){
+  return new Promise((resolve)=>{
+    stdin.on( 'data', function( key ){
+      resolve(key);
+    });
+  })
+}
+
+void async function(){
+  while(true){
+    let char = await getChar();
+    if(char === "\u0003"){
+      process.exit()
+      break;
+    }
+    console.log(char.split("").map(c=>c.charCodeAt(0)) );
+  }
+}()
