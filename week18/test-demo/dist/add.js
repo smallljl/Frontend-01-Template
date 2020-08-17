b8560dbb9719193232d3429c85ebd017bd7f79f8
smallljl <168241296@qq.com>
2020-08-17 09:38:27 +0800

    vue

--- week18/test-demo/dist/add.js
+++ week18/test-demo/dist/add.js
@@ -1,11 +1,8 @@
-"use strict"
+"use strict";
 
-Object.defineProperty(exports,"__esModule",{
-    value:true
-});
-
-exports.add = add;
-
-function add(a,b){
-    return a + b;
+function add(a, b) {
+  return a + b;
 }
+
+module.exports.add = add;
+
