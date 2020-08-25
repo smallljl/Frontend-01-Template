aa84f63eee8b38feb3fef78ac792dd2cdd1a6d3f
smallljl <168241296@qq.com>
2020-08-25 11:59:56 +0800

    publish 文件传输完成

--- week19/Publish/publish-server-vanilla/index.js
+++ week19/Publish/publish-server-vanilla/index.js
@@ -1,12 +1,16 @@
 const http = require('http');
 const fs = require("fs");
+const unzip = require("unzipper");
+
 // Create an HTTP tunneling proxy
 const server = http.createServer((req, res) => {
-  let matched = req.url.match(/filename=([^&]+)/);
-  let filename = (matched && matched[1]);
-  if(!filename)
-    return;
-  let writeStream = fs.createWriteStream("../server/public/" + filename);
+  // let matched = req.url.match(/filename=([^&]+)/);
+  // let filename = (matched && matched[1]);
+  // if(!filename)
+  //   return;
+  // let writeStream = fs.createWriteStream("../server/public/" + filename);
+
+  let writeStream = unzip.Extract({"path":"../server/public"});
 
   /*
   req.on("data",thunk=>{
--- week19/Publish/publish-tool/publish.js
+++ week19/Publish/publish-tool/publish.js
@@ -1,29 +1,32 @@
 const http = require("http");
 const querystring = require("querystring");
 const fs = require("fs");
+var archiver = require("archiver");
 
 const postData = querystring.stringify({
   "content":"鑫哥你真棒hjkdhsakjdhaskjdhksajdhkj"
 }); 
 
-let filename = "./cat.jpg";
-
+let packname = "./package";
 
 // 文件大小
-fs.stat(filename,(err,stat)=>{
-  console.log(stat.size);
+//fs.stat(filename,(err,stat)=>{
+  // console.log(stat.size);
 
   const options = {
     host:"localhost",
     port:8081,
-    path:"/a?filename="+filename,
+    path:"/a?filename="+"package.zip",
     method:"POST",
     headers:{
       'Content-Type': 'application/octet-stream',
       // 'Content-Length': Buffer.byteLength(postData)
-      'Content-Length':stat.size
+      //'Content-Length':0
     }
   };
+
+
+ 
 
   const req = http.request(options, (res) => {
     console.log(`STATUS: ${res.statusCode}`);
@@ -40,15 +43,36 @@ fs.stat(filename,(err,stat)=>{
   req.on('error', (e) => {
     console.error(`problem with request: ${e.message}`);
   });
+
   
+  var archive = archiver("zip",{
+    zlib:{level:9}
+  });
+
+  archive.directory(packname,false);
+
+  archive.finalize();
+
+  archive.on("end",()=>{
+    console.log("end");
+  });
+
+  archive.pipe(req);
+
+  archive.on("end",()=>{
+    req.end();
+  });
+  
+ /*
   let readStream = fs.createReadStream(filename);
   readStream.pipe(req);
   readStream.on("end",()=>{
     req.end();
   })
   // req.write(postData);
+  */
 
-});
+// });
 
 
 
